4711398535585ae6e0638ea0952d064bb81a69d3
David Bobe <bobe.david@live.com>
2024-05-28 10:27:21 -0400

    Local fonts start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-purple-951">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Coffee Connoisseur</h1>
+      <h2>The best coffee shops in town.</h2>
     </main>
   );
 }
